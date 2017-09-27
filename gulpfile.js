2b461c863b3b0a1d2b000d15da87af01b3af34d0
songlei <songlei@qq.com>
2017-09-27 15:25:34 +0800

    修改

--- gulpfile.js
+++ gulpfile.js
@@ -7,34 +7,34 @@ var sass = require('gulp-ruby-sass');
 
 // 新建压缩合并JS文件的任务
 gulp.task('minifyJS', function () {
-	return gulp.src('./src/js/*.js').pipe(uglify())
+	return gulp.src('./src/js.js').pipe(uglify())
 	.pipe(concat('all.js'))
 	.pipe(gulp.dest('./dist/js/'));
 });
 
 // 新建压缩合并CSS文件的任务
-gulp.task('minifyCss', function () {
-	return gulp.src('./src/style/*.css')
-	.pipe(cleanCss())
-	.pipe(concat('all.css'))
-	.pipe(gulp.dest('./dist/style/'));
-});
+//gulp.task('minifyCss', function () {
+//	return gulp.src('./src/style.css')
+//	.pipe(cleanCss())
+//	.pipe(concat('all.css'))
+//	.pipe(gulp.dest('./dist/style/'));
+//});
 
 // 操作sass源文件
 gulp.task('minifyScss', function () {
 	return sass('./src/scss/*.scss', {
 		style: 'compressed'
-	}).pipe(concat('all.css')).pipe(gulp.dest('./dist/style/'));
+	}).pipe(gulp.dest('./dist/style/'));
 });
 
 // 新建重新加载reload任务
-gulp.task('reload', ['minifyJS', 'minifyScss'], function () {
+gulp.task('reload', ['minifyScss'], function () {
 	gulp.src('./index.html').pipe(connect.reload());
 });
 
 
 // gulp允许创建一个默认的任务
-gulp.task('default', ['minifyJS', 'minifyScss'], function () {
+gulp.task('default', ['minifyScss', 'minifyJS'], function () {
 
 	// 开启服务器
 	connect.server({
@@ -46,5 +46,5 @@ gulp.task('default', ['minifyJS', 'minifyScss'], function () {
 	gulp.watch('./main.html', ['rename']);*/
 
 
-	gulp.watch(['./src/js/*.js', './src/scss/*.scss'], ['reload']);
+	gulp.watch(['./src/scss/*.scss'], ['reload']);
 });
